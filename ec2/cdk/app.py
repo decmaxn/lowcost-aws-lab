4ee8c8406a1f7c6893a1e7fabf889612627bbb6a
Victor Ma <decmaxn@hotmail.com>
2024-03-01 00:34:43 -0500

    create the similar free ec2 by cdk

--- ec2/cdk/app.py
+++ ec2/cdk/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-
 from cdk.cdk_stack import CdkStack
 
-
 app = cdk.App()
-CdkStack(app, "CdkStack")
+env = cdk.Environment(
+    account="xxxxxxxxxxxx",
+    region="us-east-1",
+)
+CdkStack(app, "cdk", env=env)
 
 app.synth()
--- ec2/cdk/cdk/cdk_stack.py
+++ ec2/cdk/cdk/cdk_stack.py
@@ -1,26 +1,53 @@
-from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_ec2 as ec2
 )
+from constructs import Construct
 
+# 从user_data.txt 文件读出 user_data
+with open('user_data.txt') as f:
+    user_data = f.read()
 
 class CdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkQueue",
-            visibility_timeout=Duration.seconds(300),
+        # Find the Default VPC
+        default_vpc = ec2.Vpc.from_lookup(
+            self, "DefaultVpc", 
+            is_default=True
         )
 
-        topic = sns.Topic(
-            self, "CdkTopic"
+        # Found the latest debian 11 arm64 AMI which could be launched as t4g.small 
+        image = ec2.LookupMachineImage(
+            owners = ['amazon'],
+            name = 'debian-11-arm64*',
+            filters={
+                'virtualization-type': ['hvm'],
+                'architecture': ['arm64']
+            },
+        )
+        
+        # Create an IAM role for SSM
+        ssm_role = iam.Role(self, "SSMInstanceRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore")]
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # Create an instance profile and add the role to it
+        ssm_profile = iam.CfnInstanceProfile(self, "SSMInstanceProfile",
+            roles=[ssm_role.role_name]
+        )
+
+        # 创建一个EC2实例, 给公共IP吧
+        instance = ec2.Instance(
+            self, "MyInstance",
+            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.SMALL),
+            vpc=default_vpc,
+            machine_image=image,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            user_data=ec2.UserData.custom(user_data),
+            role=ssm_role
+        )
